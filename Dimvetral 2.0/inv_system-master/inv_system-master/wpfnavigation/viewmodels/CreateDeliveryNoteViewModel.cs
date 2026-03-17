e6cf63dceb08cec3183fc9823b25e5abf4f65c3c
Wozar <2442498-wozar@users.noreply.gitlab.com>
2026-03-17 13:05:40 +0100

    Add CreateDeliveryNoteViewModel with navigation and command setup

---
+++ Dimvetral 2.0/inv_system-master/inv_system-master/wpfnavigation/viewmodels/CreateDeliveryNoteViewModel.cs
@@ -0,0 +1,35 @@
+using System.Windows.Input;
+using wpfnavigation.Commands;
+using wpfnavigation.Models.Repositories;
+using wpfnavigation.Services;
+using wpfnavigation.Stores;
+
+namespace wpfnavigation.viewmodels;
+
+public class CreateDeliveryNoteViewModel : BaseViewModel
+{
+    public ICommand NavigateToHomeViewCommand { get; set; }
+    public ICommand CreateDeliveryNoteCommand { get; set; }
+
+    private string _name;
+
+    public CreateDeliveryNoteViewModel(NavigationStore navigationStore, DeliveryNoteRepository repository)
+    {
+        // NavigateToHomeViewCommand = new NavigateToHomeViewCommand(navigationStore);
+        //NavigateToHomeViewCommand = new NavigateCommand(navigationStore, () => new HomeViewModel(navigationStore));
+        CreateDeliveryNoteCommand = new CreateDeliveryNoteCommand(this, repository, navigationStore);
+        NavigateToHomeViewCommand =
+            new NavigateCommand(new NavigationService(navigationStore,
+                () => new ListingViewModel(navigationStore, repository)));
+    }
+
+    public string Name
+    {
+        get => _name;
+        set
+        {
+            _name = value;
+            OnPropertyChanged(nameof(Name));
+        }
+    }
+}
